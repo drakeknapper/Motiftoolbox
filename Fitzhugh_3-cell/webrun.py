106842cf23577854ab4ea54031949b47188ae6d9
pusulurikrishna <pusuluri.krishna@gmail.com>
2014-10-29 02:43:05 -0400

    Fitzhugh 3cell with all plugins for web

--- Fitzhugh_3-cell/webrun.py
+++ Fitzhugh_3-cell/webrun.py
@@ -1,68 +1,108 @@
-#! /usr/bin/python
+#!/usr/bin/env python
 
 
-import matplotlib.pyplot as plt, mpld3 as m
-from flask import Flask
-
+import mpld3 as m
+from mpld3 import plugins
+from flask import Flask,request
+import pylab as pl
 import system as sys
-import network3N as netw
 import info as nf
+import network3N as netw
 import torus as tor
 import traces as tra
+from WebSupport.Plugins.clickPlugin import ClickPlugin
+from WebSupport.Plugins.dragPlugin import DragPlugin
+from matplotlib.backend_bases import Event
+
 
 app = Flask(__name__)
 
+i = nf.info()
+n = netw.network(info=i)
+system = sys.system(info=i, network=n)
+traces = tra.traces(system, n, info=i)
+torus = tor.torus(system, n, traces, info=i)
+n.system = system
+system.traces = traces
 
-pos_info = '+0+600'
-pos_tra = '+300+600'
-pos_net = '+300+0'
-pos_sys = '+0+0'
-pos_torus = '+800+0'
 
-i = nf.info(position=pos_info)
-n = netw.network(info=i, position=pos_net)
-s = sys.system(info=i, position=pos_sys, network=n)
-n.system = s
-t = tra.traces(s, n, info=i, position=pos_tra)
-tr = tor.torus(s, n, t, info=i, position=pos_torus)
+plugins.connect(torus.fig, ClickPlugin(eventHandlerURL="updatetorus",radioButtonID="torusRadio"))
+plugins.connect(system.fig, DragPlugin(eventHandlerURL="updatesystem",radioButtonID="systemRadio"))
+plugins.connect(n.fig, DragPlugin(eventHandlerURL="updatenetwork",radioButtonID="networkRadio"))
+
+sweepphasespace = False;
+
+
 
 @app.route("/")
 def hello():
-
-    x1=m.fig_to_html(n.fig)
-    x2=m.fig_to_html(s.fig)
-    x3=m.fig_to_html(i.fig)
-    x4=m.fig_to_html(t.fig)
-    x5=m.fig_to_html(tr.fig)
-    x=x1+"\n"+x2+"\n"+x3+"\n"+x4+"\n"+x5+"\n"
-    #print "HTML output : \n",x;
-    #return "Hello World"
-    return x;
-
-
-@app.route("/test")
-def hello1():
-    x = '<h1 style="color:red">The Main Page that opens other plots</h1> \n' \
-        '<div id="systemframe" style="position:relative; top: 20px; left: 0px;">' \
-        '<iframe id="system" style="border-style: none; border-color: red; border-width: 1px; height:500px; width:500px" src="/system"></iframe>' \
-        '</div' \
-        '<div id="networkframe" style="position:relative; top: 20px; right: 0px;">' \
-        '<iframe id="network" style="border-style: none; border-color: red; border-width: 1px; height:500px; width:500px;" src="/network"></iframe>'\
-        '</div'
-    return x;
+	f = open('fitzhugh_3cell.html','r')
+	HTML = f.read();
+	return HTML
 
 @app.route("/system")
 def fun1():
-    x2=m.fig_to_html(s.fig)
-    return x2;
+	return m.fig_to_html(system.fig)
+
+@app.route("/torus")
+def fun2():
+	return m.fig_to_html(torus.fig)
 
 @app.route("/network")
-def fun2():
-    x1=m.fig_to_html(n.fig)
-    return x1;
+def fun3():
+	return m.fig_to_html(n.fig)
+
+@app.route("/traces")
+def fun4():
+	return m.fig_to_html(traces.fig)
+
+@app.route("/info")
+def fun5():
+	return m.fig_to_html(i.fig)
+
+@app.route("/pylabshow")
+def fun6():
+	pl.show()
+	return
+
+@app.route("/updatetorus")
+def fun7():
+	if request.args['type']=='sweep':
+		global sweepphasespace
+		if sweepphasespace==False :
+			torus.sweep_phase_space()
+			sweepphasespace = True
+		return
+	elif request.args['type']=='trace':
+		print request.args['xval'],request.args['yval']
+		torus.click_traces(float(request.args['xval']), float(request.args['yval']))
+		return
+	return
+
+@app.route("/updatesystem")
+def fun8():
+	event = Event("mockEvent",system.fig.canvas);
+	event.xdata = float(request.args['startX'])
+	event.ydata = float(request.args['startY'])
+	system.on_button(event)
+	event.xdata = float(request.args['endX'])
+	event.ydata = float(request.args['endY'])
+	event.button = int(request.args['type'])
+	system.off_button(event)
 
 
+@app.route("/updatenetwork")
+def fun9():
+	event = Event("mockEvent",n.fig.canvas);
+	event.xdata = float(request.args['startX'])
+	event.ydata = float(request.args['startY'])
+	event.button = int(request.args['type'])
+	n.on_button(event)
+	event.xdata = float(request.args['endX'])
+	event.ydata = float(request.args['endY'])
+	event.button = int(request.args['type'])
+	n.off_button(event)
 
 if __name__ == "__main__":
-    app.run()
-    #hello();pl.show()
+	app.run()
+
--- Tools/WebSupport/Plugins/dragPlugin.py
+++ Tools/WebSupport/Plugins/dragPlugin.py
@@ -1,9 +1,4 @@
 import os.path
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
-
-import mpld3
 from mpld3 import plugins, utils
 
 
@@ -11,27 +6,8 @@ class DragPlugin(plugins.PluginBase):
     f=open(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))+"/javascripts/dragPlugin.js",'r');
     JAVASCRIPT = f.read()
     f.close()
-
-
-    def __init__(self, points):
-        if isinstance(points, mpl.lines.Line2D):
-            suffix = "pts"
-        else:
-            suffix = None
-
+    def __init__(self, eventHandlerURL, radioButtonID):
         self.dict_ = {"type": "drag",
+                      "eventHandlerURL": eventHandlerURL,
+                      "radioButtonID": radioButtonID
                      }
-
-
-if __name__ == "__main__" :
-
-    fig, ax = plt.subplots()
-    np.random.seed(0)
-    points = ax.plot(np.random.normal(size=20),
-                     np.random.normal(size=20), 'or', alpha=0.5,
-                     markersize=50, markeredgewidth=1)
-    ax.set_title("Click and Drag", fontsize=18)
-
-    plugins.connect(fig, DragPlugin(points[0]))
-
-    mpld3.show()
