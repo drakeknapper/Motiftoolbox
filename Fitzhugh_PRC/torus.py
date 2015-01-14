b300932a955cc3fe3c76c46d4646efe14ffe3913
Justus Schwabedal <jschwabedal@gmail.com>
2015-01-14 13:48:45 -0500

    Try To finish merge.

---
+++ Fitzhugh_PRC/torus.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+import sys
+sys.path.insert(0, '../Tools')
+import fitzhugh as model
+import prcNetwork
+import torus_2D
+import numpy as np
+
+
+
+class torus(torus_2D.torus_2D):
+
+        model = model
+        V_trigger = 0.
+	figsize = (13, 6.5)
+
+	def __init__(self, system, network, traces, info=None, position=None):
+
+                torus_2D.torus_2D.__init__(self, system, network, traces, info, position)
+		self.quiver = None
+
+
+	def erase_traces(self):
+		torus_2D.torus_2D.erase_traces(self)
+
+		if self.quiver:
+			self.quiver.remove()
+			self.quiver = None
+                
+
+	def vectorField_prc(self):
+
+		self.erase_traces()
+
+		phase, coupling = self.system.threeCellCoupling(0.03, self.network.coupling_strength)
+		coupling_function = prcNetwork.interp_torus_vec(phase, phase, coupling)
+		coupling_function.findRoots(GRID=self.GRID)
+		self.quiver = coupling_function.plot(self.GRID, self.ax_traces, period=1.)
+
+		self.fig.canvas.draw()
+
+
+
+
+	
+
+
+
+if __name__ == "__main__":
+
+	import pylab as pl
+	import system as sys
+	import network3N as netw
+	import traces as tra
+	import info as nf
+		
+	info = nf.info()
+	system = sys.system(info=info)
+	network = netw.network(info=info)
+	traces = tra.traces(system, network, info=info)
+	t = torus(system, network, traces, info=info)
+	system.torus = t
+	t.vectorField_prc()
+
+	pl.show()
+
+
+
+
+
---
+++ Fitzhugh_PRC/traces.py
@@ -0,0 +1,132 @@
+#!/usr/bin/env python
+
+import time
+import sys
+sys.path.insert(0, '../Tools')
+import fitzhugh as fh
+import tools as tl
+import window as win
+
+import numpy as np
+import pylab as pl
+
+class traces(win.window):
+
+	title = 'Voltage Traces'
+	figsize = (13, 2)
+
+	def __init__(self, system, network, info=None, position=None):
+		win.window.__init__(self, position)
+		self.system = system
+		self.network = network
+		self.info = info
+		self.CYCLES = 20
+
+		self.ax = self.fig.add_subplot(111, frameon=False, yticks=[])
+
+		self.li_b, = self.ax.plot([], [], 'b-', lw=2.)
+		self.li_g, = self.ax.plot([], [], 'g-', lw=2.)
+		self.li_r, = self.ax.plot([], [], 'r-', lw=2.)
+
+		self.ax.set_xlabel(r'time (sec.)', fontsize=20)
+
+		self.ax.set_xticklabels(np.arange(0., 1., 0.1), fontsize=15)
+		self.ax.set_yticklabels(np.arange(0., 1., 0.1), fontsize=15)
+		
+		self.ax.set_xlim(0., 100.)
+		self.ax.set_ylim(-5.5, 1.5)
+
+		#self.fig.tight_layout()
+
+		self.key_func_dict = dict(u=traces.increaseCycles, i=traces.decreaseCycles)
+		self.fig.canvas.mpl_connect('axes_enter_event', self.focusIn)
+
+		self.computeTraces()
+
+
+	def adjustCycles(self, adjustment):
+		self.CYCLES = adjustment
+		self.computeTraces()
+		self.focusIn()
+
+
+	def increaseCycles(self):
+		self.adjustCycles(self.CYCLES+1)
+
+	def decreaseCycles(self):
+		self.adjustCycles(self.CYCLES-1*(self.CYCLES>0))
+
+
+	def focusIn(self, event=None):
+		descriptor = "CYCLES : "+str(self.CYCLES)+" ('u' > 'i')"
+
+		if self.info == None:
+			print descriptor
+
+		else:
+			self.info.set(descriptor)
+
+
+	def on_key(self, event):
+
+		try:
+			self.key_func_dict[event.key](self)
+
+		except:
+			self.key_func_dict[event.key] = lambda x: None
+
+
+	def computeTraces(self, initial_condition=None, plotit=True):
+
+		if initial_condition == None:
+			initial_condition = self.system.load_initial_condition(pl.rand(), pl.rand())
+
+		V_i = fh.integrate_three_rk4(
+				initial_condition,
+				self.network.coupling_strength,
+				self.system.dt/float(self.system.stride),
+				self.system.N_output(self.CYCLES),
+				self.system.stride)
+
+		t = self.system.dt*np.arange(V_i.shape[0])
+
+		if plotit:
+			ticks = np.asarray(t[::t.size/10], dtype=int)
+
+			xscale, yscale = t[-1], 2.
+			for (i, li) in enumerate([self.li_b, self.li_g, self.li_r]):
+				tj, Vj = tl.adjustForPlotting(t, V_i[:, i], ratio=xscale/yscale, threshold=0.05*xscale)
+				li.set_data(tj, Vj-i*2)
+
+			self.ax.set_xticks(ticks)
+			self.ax.set_xticklabels(ticks)
+			self.ax.set_xlim(t[0], t[-1])
+			self.fig.canvas.draw()
+
+		return t, V_i
+
+
+
+
+
+
+	
+
+
+
+if __name__ == "__main__":
+
+	import system as sys
+	import network as netw
+		
+	s = sys.system()
+	n = netw.network()
+	
+
+	tra = traces(s, n)
+
+
+	pl.show()
+
+
+
